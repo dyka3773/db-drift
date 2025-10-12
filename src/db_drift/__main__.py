fe33ece1520879f8257fdb3b364339a9a0cecbd9
Hercules Konsoulas <dyka3773@gmail.com>
2025-10-12 17:02:34 +0200

    feat: add logging configuration

--- src/db_drift/__main__.py
+++ src/db_drift/__main__.py
@@ -1,7 +1,12 @@
 from db_drift.cli.cli import cli
+from db_drift.utils import custom_logging
+
+logger = custom_logging.setup_logger("db-drift")
 
 
 def main() -> None:
+    """Entry point for the db-drift package."""
+    logger.debug("Starting db-drift CLI")
     cli()
 
 
