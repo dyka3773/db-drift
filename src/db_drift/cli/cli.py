50ea4c39a69c75085dedcc20f8d1a3b6ddb95f2c
Hercules Konsoulas <dyka3773@gmail.com>
2025-10-12 15:39:53 +0200

    feat(cli): add CLI version info
    Relates to #67

--- src/db_drift/cli/cli.py
+++ src/db_drift/cli/cli.py
@@ -1,10 +1,19 @@
 import argparse
 
+from db_drift.cli.utils import get_version
+
 
 def cli() -> None:
     parser = argparse.ArgumentParser(
         prog="db-drift",
         description="A command-line tool to visualize the differences between two DB states.",
+    )
+
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=f"db-drift {get_version()}",
     )
 
     args = parser.parse_args()
