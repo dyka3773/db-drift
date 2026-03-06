275d7f5fd63a55236e05483aa9edd35eb653c9c7
Hercules Konsoulas <dyka3773@gmail.com>
2026-03-06 22:08:37 +0100

    perf(oracle): add a more robust algorithm for hashing code text

--- src/db_drift/utils/string.py
+++ src/db_drift/utils/string.py
@@ -3,12 +3,12 @@ import hashlib
 
 def hash_body(body: str) -> str:
     """
-    Generate an MD5 hash of the given body string.
+    Generate a SHA-256 hash of the given body string.
 
     Args:
         body (str): The body string to hash.
 
     Returns:
-        str: The MD5 hash of the body.
+        str: The SHA-256 hash of the body.
     """
-    return hashlib.md5(body.encode("utf-8")).hexdigest()  # noqa: S324
+    return hashlib.sha256(body.encode("utf-8")).hexdigest()
